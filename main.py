7a7eb231be0f5f39da459fe32604725fa661b964
Maxim Kachura <maksimkacura25@gmail.com>
2026-04-26 18:20:37 +0200

    init bot structure

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-#  определиться с необходимыми библиотеками
-#  подготовить окружение
+from bot import bot
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, 'Бот работает 🚀')
+
+if __name__ == '__main__':
+    print('Бот запущен!')
+    bot.infinity_polling()
+
